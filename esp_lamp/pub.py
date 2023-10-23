b19dcb9b26dd8c7771f00ef4247bccbcdd9bee2d
vladnov138 <vladnov138@gmail.com>
2023-10-23 15:57:18 +0800

    Add questionary lib

--- esp_lamp/pub.py
+++ esp_lamp/pub.py
@@ -1,6 +1,7 @@
 import time
 
 import paho.mqtt.client as paho
+import questionary
 
 broker = 'broker.emqx.io'
 
@@ -11,18 +12,23 @@ client.connect(broker)
 client.loop_start()
 print("Publishing")
 
-# cmd = input()
-start_time = 40
+topic = questionary.text("What's topic?").ask()
+print(topic)
+
+start_time = 20
 cur_time_limit = start_time
-min_time = 20
+min_time = 10
 while True:
-    client.publish("house/bulb1", "1")
+    client.publish(topic, "0")
+    print(0)
+    time.sleep(20)
+    client.publish(topic, "1")
     print(1)
     time.sleep(cur_time_limit)
-    client.publish("house/bulb1", "0")
+    client.publish(topic, "0")
     print(0)
-    time.sleep(60 - cur_time_limit)
-    if cur_time_limit > 20:
+    time.sleep(40 - cur_time_limit)
+    if cur_time_limit > min_time:
         cur_time_limit -= 1
     else:
         cur_time_limit = start_time
